cde10519e83a95e735da741b54f7c612dab03c85
Grigory Yaufmann <Yaufmannnnn@yandex.ru>
2024-12-10 22:39:04 +0300

    tasks7_update

--- task7.py
+++ task7.py
@@ -1,16 +1,14 @@
 #Задание 7
 
 n = int(input("Введите число: "))
-
 summ = 1
 current = 1
-
+print(f"Факториал числа 1! равен {current}")
 for i in range(2,n+1):
-
    current *= i
    summ += current
-
-print(summ)   
+   print(f"Факториал числа {i}! равен {current}")   
+print(f"Общая сумма факториалов: {summ}")   
 
 
      
