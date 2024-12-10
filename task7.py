c6e0f7c51e152173e2535c87ff6fd329154b3927
Grigory Yaufmann <Yaufmannnnn@yandex.ru>
2024-12-10 22:15:54 +0300

    tasks7

---
+++ task7.py
@@ -0,0 +1,23 @@
+#Задание 7
+
+n = int(input("Введите число: "))
+
+summ = 1
+current = 1
+
+for i in range(2,n+1):
+
+   current *= i
+   summ += current
+
+print(summ)   
+
+
+     
+   
+
+      
+      
+      
+
+
