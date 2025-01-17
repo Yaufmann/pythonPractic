1e50463d4d69a25fbf2a45d1270994a3136ef2b4
Grigory Yaufmann <Yaufmannnnn@yandex.ru>
2025-01-17 19:40:34 +0300

    +folder

---
+++ practicEX/practic5.py
@@ -0,0 +1,13 @@
+def searchLetter(string): 
+
+   arr = string.split(' ')
+   count = 0
+   for element in range(0,len(arr)):
+      
+      if arr[element][0] == 'е':
+         count += 1
+   
+   return print(f'Количество слов начинающихся с буквы - е: {count}')
+
+
+searchLetter('Строка со словом екид и словом ехон!')
