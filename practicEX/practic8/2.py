4d0e2fdc01fda1beaf6746b6c63f586230b2ee10
Yaufmann <Yaufmannnnn@yandex.ru>
2025-01-20 23:01:13 +0300

    WW

--- practicEX/practic8/2.py
+++ practicEX/practic8/2.py
@@ -1,32 +1,31 @@
 A = int(input('Введите количество строк: '))
 B = int(input('Введите количество столбцов: '))
 
-C = []
-for i in range(A):
-    D = []
-    for j in range(B):
-        D.append(int(input('Введите число: ')))
-    C.append(D)
+def matrixElement(A,B):
+    C = []
+    for i in range(A):
+        D = []
+        for j in range(B):
+            D.append(int(input('Введите число: ')))
+        C.append(D)
 
-print(type(C[i]))
-print(max(C[0]))
-for i in range(A):
-    maxElementString = max(C[i])
-    minElementString = min(C[i])
+    for i in range(A):
+        maxElementString = max(C[i])
+        minElementString = min(C[i])
 
-    for j in range(B):
-        if C[i][j] == C[i][0]:
-            index = C[i].index(maxElementString)
-            #C[i].insert(index,C[i][0])
-            C[i][index] = C[i][0]
-            C[i][0] = maxElementString
-        if C[i][j] == C[i][-1]:
-            index = C[i].index(minElementString)
-            #C[i].insert(index,C[i][-1])
-            C[i][index] = C[i][-1]
-            C[i][0] = minElementString
+        indexMax = C[i].index(maxElementString)
+        C[i][indexMax] = C[i][0]
+        C[i][0] = maxElementString
 
-for i in range(A):
-    for j in range(B):
-        print(C[i][j], end=' ')
-    print()
+        indexMin = C[i].index(minElementString)
+        C[i][indexMin] = C[i][-1]
+        C[i][-1] = minElementString
+
+    for i in range(A):
+        for j in range(B):
+            print(C[i][j], end=' ')
+        print()
+    
+    return C
+
+print(matrixElement(A,B))
